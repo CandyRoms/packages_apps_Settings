6d8c4fad1185cb3e8c0b2682ba1ba3a5a28aceb8
NoSpamDan <nospamdan1@gmail.com>
2020-11-21 00:03:12 -0500

    doze: Clean up double tap settings
    Change-Id: I27e9eacf4020a52439194b314feaa5af01f90b88

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -30,7 +30,7 @@ import com.android.settings.display.AmbientDisplayNotificationsPreferenceControl
 import com.android.settings.display.AmbientWakeGesturesPreferenceController;
 import com.android.settings.display.PulseOnNewTracksPreferenceController;
 import com.android.settings.display.AmbientDisplayCustomPreferenceController;
-import com.android.settings.gestures.DoubleTapScreenPreferenceController;
+//import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
 import com.android.settings.notification.LockScreenNotificationPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -99,7 +99,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         use(PulseOnNewTracksPreferenceController.class).setConfig(getConfig(context));
         use(AmbientDisplayNotificationsPreferenceController.class).setConfig(getConfig(context));
         use(AmbientWakeGesturesPreferenceController.class).setConfig(getConfig(context));
-        use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
+        //use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
         addPreferenceController(new AmbientDisplayCustomPreferenceController(context));
     }
